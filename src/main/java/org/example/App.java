5cf78628ca6e7724a2baf3081d4695289c04a6ed
Brandon Knudson <bknudson117@gmail.com>
2021-06-06 08:21:10 -0400

    1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        double tax = 0.055, subtotal = 0, temp, tax_amount;
+        String collector;
+        int counter =1;
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.printf("Enter the price of item %d: ", counter);
+            collector = input.nextLine();
+            temp = Double.valueOf(collector);
+            System.out.printf("Enter the quantity of item %d: ", counter);
+            collector = input.nextLine();
+            temp = temp * Double.valueOf(collector);
+
+            subtotal += temp;
+            counter += 1;
+        }
+
+        tax_amount = subtotal * tax;
+        System.out.printf("Subtotal: $%.2f%n", subtotal);
+        System.out.printf("Tax: $%.2f%n", tax_amount);
+        System.out.printf("Total: $%.2f%n", subtotal + tax_amount);
+
+    }
+}
